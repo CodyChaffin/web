0fac25d1cc970612f5b6dc2376928808308edf2f
cody <cmchaff14@gmail.com>
2021-08-25 17:40:44 -0500

    check

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 const h2 = document.createElement('h2');
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "Life has thrown some curve balls";
 document.querySelector('body').appendChild(h2);
+const h3 = document.createElement('h3')
+h3.textContent = "At 18 I had my first son"
+document.querySelector('body').append(h3)
